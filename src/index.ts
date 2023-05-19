e7d2d09c332355ca61da2f437ccef57a65e6f9e1
wss <wssgryx@163.com>
2023-05-19 18:02:01 +0800

    feat: ✨ init repo
    Signed-off-by: wss <wssgryx@163.com>

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { lodash as _, Logger, getInputs, getStepContext, getCredentials } from '@serverless-cd/core';
+import Cache, { IProps, ICredentials } from './cache';
+
+interface ISchemaError {
+  error: Error;
+};
+
+const getCacheInputs = async (inputs: Record<string, any>, context: Record<string, any>, logger: Logger): Promise<IProps | ISchemaError> => {
+  logger.debug(`context: ${JSON.stringify(context)}`);
+  logger.debug(`inputs: ${JSON.stringify(inputs)}`);
+  const newInputs = getInputs(inputs, context) as Record<string, any>;
+  logger.debug(`newInputs: ${JSON.stringify(newInputs)}`);
+
+  const credentials = await getCredentials(newInputs, context) as ICredentials; // 此处预期是可以获取应用中心注入的 sts 的数据
+  const currentRegion = _.get(context, 'inputs.currentRegion');
+  const region = _.get(context, 'inputs.ctx.data.cacheConfig.oss.regionId', currentRegion);
+  credentials.accountId = credentials.accountId || _.get(context, 'inputs.uid');
+
+  return {
+    region,
+    cwd: _.get(context, 'cwd', process.cwd()),
+    objectKey: _.get(newInputs, 'key', ''),
+    cachePath: _.get(newInputs, 'path', ''),
+    bucket: _.get(context, 'inputs.ctx.data.cacheConfig.oss.bucketName', ''),
+    internal: currentRegion === region,
+    credentials,
+  };
+}
+
+export const run = async (inputs: Record<string, any>, context: Record<string, any>, logger: Logger) => {
+  logger.info('start @serverless-cd/cache run');
+  const props = await getCacheInputs(inputs, context, logger);
+  if ((props as ISchemaError).error) {
+    const error = _.get(props, 'error') as unknown as Error;
+    logger.warn(`The entry information is wrong: ${error.message}`);
+    return { 'cache-hit': false, error };
+  }
+  const cache = new Cache(props as IProps, logger);
+  const res = await cache.run();
+  logger.info('Run @serverless-cd/cache end');
+  return res;
+};
+
+export const postRun = async (inputs: Record<string, any>, context: Record<string, any>, logger: Logger) => {
+  logger.info('start @serverless-cd/cache postRun');
+  const props = await getCacheInputs(inputs, context, logger);
+  if ((props as ISchemaError).error) {
+    const error = _.get(props, 'error') as unknown as Error;
+    logger.warn(`The entry information is wrong: ${error.message}`);
+    return;
+  }
+
+  const stepContext = getStepContext(context);
+  const cacheHit = _.get(stepContext, 'run.outputs.cache-hit');
+  const cacheError = _.get(stepContext, 'run.outputs.error');
+  logger.debug(`Get run output cache hit: ${cacheHit}`);
+  const cache = new Cache((props as IProps), logger);
+  await cache.postRun(cacheHit, cacheError);
+  logger.info('postRun @serverless-cd/cache end');
+};
+
+export default Cache;
